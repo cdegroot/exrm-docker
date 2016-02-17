7d4d8698746269d5284c1e9f3bfe867d6f35ae5d
Cees de Groot <cees@pagerduty.com>
2016-02-17 17:23:36 -0500

    Prevent beam from hanging

--- mix-release.sh
+++ mix-release.sh
@@ -3,5 +3,8 @@ home=$(cd $(dirname $0); /bin/pwd)
 version=$(cat $home/version.txt)
 tag=$(cat $home/tag.txt)
 
-docker run -i -t --rm -v $(pwd):$(pwd) -w $(pwd) $tag:$version mix release.clean
-docker run -i -t --rm -v $(pwd):$(pwd) -w $(pwd) $tag:$version mix release
+# Run it through bash to make bash PID 1. See https://github.com/edevil/docker-erlang-bug#explanation
+# for what's going on. The 1>&1 apparently forces Bash to fork-before-exec, trick stolen
+# from https://github.com/elixir-lang/elixir/issues/3342
+docker run -i -t --rm -v $(pwd):$(pwd) -w $(pwd) $tag:$version bash -c "mix release.clean 1>&1"
+docker run -i -t --rm -v $(pwd):$(pwd) -w $(pwd) $tag:$version bash -c "mix release 1>&1"
