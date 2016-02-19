f5640730db26f469ae0d1651b56fa14e0c25480a
Cees de Groot <cees@pagerduty.com>
2016-02-19 10:06:03 -0500

    Remove -i and -t opts, they are unnecessary and cause problems

--- mix-release.sh
+++ mix-release.sh
@@ -6,5 +6,5 @@ tag=$(cat $home/tag.txt)
 # Run it through bash to make bash PID 1. See https://github.com/edevil/docker-erlang-bug#explanation
 # for what's going on. The 1>&1 apparently forces Bash to fork-before-exec, trick stolen
 # from https://github.com/elixir-lang/elixir/issues/3342
-docker run -i -t --rm -v $(pwd):$(pwd) -w $(pwd) $tag:$version bash -c "mix release.clean 1>&1"
-docker run -i -t --rm -v $(pwd):$(pwd) -w $(pwd) $tag:$version bash -c "mix release 1>&1"
+docker run --rm -v $(pwd):$(pwd) -w $(pwd) $tag:$version bash -c "mix release.clean 1>&1"
+docker run --rm -v $(pwd):$(pwd) -w $(pwd) $tag:$version bash -c "mix release 1>&1"
